6e4238e4da71357a3859a75ad6ebb253201385c4
Sumanth VS <53754306+1BM18CS113@users.noreply.github.com>
2019-09-30 15:43:09 +0530

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -12,16 +12,19 @@ NODE getnode();
 NODE insert_beg(NODE first, int ele);
 NODE insert_end(NODE first, int ele);
 NODE display(NODE first);
-
+NODE insert_any_pos(NODE first, int ele, int pos);
+NODE delete_first(NODE first);
+NODE delete_end(NODE first);
+NODE delete_any_pos(NODE first, int pos);
 
 void main(){
 	
-	int choice, ele;
+	int choice, ele, pos;
 	NODE first = NULL;
 	
 	
 	do{
-		printf("1. Insert at the front. 2. Insert at end. 3. Display. 5. Exit\n");
+		printf("1. Insert at the front. \n2. Insert at end. \n3. Display. \n4. Insert at any pos. \n5. Delete from FIRST \n6. DElete END \n7. Exit. \n8. Delete Any pos\n");
 		scanf("%d", &choice);
 		
 		switch(choice){
@@ -42,8 +45,32 @@ void main(){
 			case 3:
 			first = display(first);
 			break;
+			
+			case 4:
+			printf("ENTER THE ELEMENT TO INSERT AT THE END\n");
+			scanf("%d", &ele);
+			
+			printf("ENTER THE Position TO INSERT\n");
+			scanf("%d", &pos);
+			
+			first = insert_any_pos(first, ele, pos);
+			break;
+			
+			case 5:
+			first = delete_first(first);
+			break;
+			
+			case 6:
+			first = delete_end(first);
+			break;
+			
+			case 8:
+			printf("ENTER THE Position TO DELETE\n");
+			scanf("%d", &pos);
+			first = delete_any_pos(first, pos);
+			break;
 		}
-	}while(choice !=5);
+	}while(choice !=7);
 }
 
 NODE getnode(){
@@ -88,11 +115,56 @@ NODE insert_end(NODE first, int ele){
 	return first;
 }
 
+NODE insert_any_pos(NODE first, int ele, int pos){
+	
+	NODE temp = getnode();
+	temp->val = ele;
+	NODE curr = first;
+	NODE prev = first;
+	int count = 1;
+	
+	if(first == NULL){
+		first = temp;
+	}
+	else{
+		while(count < pos && curr != NULL){
+			prev = curr;
+			curr = curr->next;
+			count++;
+		}
+		prev->next = temp;
+		temp->next = curr;
+		
+	}
+	return first;
+}
+
+NODE delete_first(NODE first){
+	
+	if(first == NULL){
+		printf("LIST EMPTY\n");
+		return first;
+	}
+	
+	else{
+		NODE temp = first;
+		first = first->next;
+		free(temp);
+		return first;
+	}
+	
+	return first;
+
+}
+
+
+
 NODE display(NODE first){
 	NODE temp = first;
 	
 	if(first == NULL){
 		printf("LIST EMPTY\n");
+		return first;
 	}
 	
 	while(temp->next != NULL){
@@ -102,4 +174,64 @@ NODE display(NODE first){
 	printf("%d\n", temp->val);
 	
 	return first;
+}
+
+NODE delete_end(NODE first){
+	
+	NODE temp, prev;
+	prev = first;
+	temp =first;
+	int flag =0;
+	
+	if(first == NULL){
+		printf("LIST EMPTY\n");
+		return first;
+	}
+	else{
+		while(temp->next != NULL){
+			flag =1;
+			prev = temp;
+			temp = temp->next;			
+		}
+		if(flag==1){
+			prev->next = NULL;
+			free(temp);		
+			return first;
+		}
+		else{
+			free(temp);
+			first = NULL;
+			return first;
+		}
+		
+	}
+	
+	
+}
+
+NODE delete_any_pos(NODE first, int pos){
+	
+	NODE temp = first;
+	NODE prev = first;
+	int count = 1;
+	
+	if(first == NULL){
+		printf("LIST EMPTY\n");
+	}
+	else if(pos == 1){
+		first = first->next;
+		free(prev);
+		return first;
+	}
+	else{
+		while(count<pos && temp->next!=NULL){
+			prev= temp;
+			temp = temp->next;
+			count++;
+		}
+		prev->next = temp->next;
+		free(temp);
+		return first;
+	}
+	
 }
